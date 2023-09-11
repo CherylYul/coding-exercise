2292f14db73b1b67c24c79e6510f2423dc0920f5
cherylyul <yulcheryl2357@gmail.com>
2023-09-11 15:56:31 +0700

    add bankAccount class

---
+++ Csharp/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp
+{
+    internal class BankAccount
+    {
+        public static int AccountNumberSeed = 12800782;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        public BankAccount(string owner,decimal balance)
+        {
+            this.Owner = owner;
+            this.Balance = balance;
+            this.Number = AccountNumberSeed.ToString();
+        }
+    }
+}
