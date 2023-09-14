5a82a548825ced4d4cd90ced56e1e97fc014444e
cherylyul <yulcheryl2357@gmail.com>
2023-09-15 00:45:51 +0700

    add saving account

--- Csharp/BankAccount.cs
+++ Csharp/BankAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,10 +44,10 @@ namespace SuperBank
                 throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
             if (amount > this.Balance)
                 throw new InvalidOperationException("Insufficient funds for this withdrawal");
-            var withdrawal = new Transaction(-amount, date, note); 
+            var withdrawal = new Transaction(-amount, date, note);
             allTransactions.Add(withdrawal);
         }
-        
+
         public string GetTransactionHistory()
         {
             StringBuilder sb = new StringBuilder();
@@ -55,6 +56,43 @@ namespace SuperBank
                 sb.AppendLine($"{item.Date}\t{item.Amount}\t{item.Note}");
             sb.AppendLine($"Remains:\t\t{this.Balance}");
             return sb.ToString();
+        }
+    }
+    internal class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Note { get; }
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Note = note;
+        }
+    }
+
+    internal class SavingsAccount
+    {
+        public static double currInterestRate = 0.04;
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+
+        public static double InterestRate
+        {
+            get { return currInterestRate; }
+            set { currInterestRate = value; }
+        }
+
+        // A static constructor! can't overload, no parameters, and only created 1 time.
+        static SavingsAccount()
+        {
+            Console.WriteLine("In static ctor!");
+            currInterestRate = 0.04;
+        }
+        public SavingsAccount(decimal amount)
+        {
+            Amount = amount;
+            Date = DateTime.Now;
         }
     }
 }
